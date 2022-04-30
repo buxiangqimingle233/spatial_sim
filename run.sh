1c528a0bb49667edc5ac500808ac537b8337b8ff
wangzhao <865789618@qq.com>
2022-04-30 20:12:02 +0800

    update noc submodule version: fix sw_allocator bugs

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@
 # build/bin/spatialsim tasks/4-core-1-gemm/spatial_spec
 # build/bin/spatialsim tasks/4-core-p-c/spatial_spec
 # build/bin/spatialsim tasks/9-core-1-conv/spatial_spec
-build/bin/spatialsim tasks/bert/spatial_spec
-# build/bin/spatialsim tasks/bert_inception/spatial_spec
+# build/bin/spatialsim tasks/bert/spatial_spec
+build/bin/spatialsim tasks/bert_inception/spatial_spec
